a89197d02a969235feb0c00a2b23d73e3fb25055
Peamseye <aispotato587@gmail.com>
2022-03-09 14:44:51 +0100

    Save3

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 let clicked = 0;
+
+
 let label = document.querySelector(".aim-text");
-
-
 let label2 = document.querySelector(".point-btn");
 
 label2.addEventListener("click", handleClick);
 
 function handleClick() {
     clicked+=1;
-    label.innerHTML = `Clicked: ${clicked}`;
+    label.innerHTML = `Clicked: ${clicked} | Time left: __`;
 }
+
