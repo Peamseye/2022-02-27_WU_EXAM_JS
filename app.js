1a05fedc83e80892ab759fdf94d3513068d1adbb
Peamseye <aispotato587@gmail.com>
2022-03-09 20:50:12 +0100

    Save8

--- app.js
+++ app.js
@@ -16,16 +16,19 @@ var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
             timer--;
             label2.innerHTML = `Time left:${timer}`;
         }
+        else if (clicked > 0){
+            label2.innerHTML = "EXPIRED";
+        }
         
 
     
       document.getElementById("time").innerHTML = timer;
         
       // If the count down is over, write some text 
-      if (timer <= 0) {
+      //if (timer <= 0) {
         //clearInterval(x);
         //document.getElementById("time").innerHTML = "EXPIRED";
-    }
+    //}
 
     }, 1000);
 
