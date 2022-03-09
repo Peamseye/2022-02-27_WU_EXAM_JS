22f8b19133a516d0e35569dd2fe3e1b591743ad6
Peamseye <aispotato587@gmail.com>
2022-03-09 20:42:27 +0100

    Save6

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-let clicked = 0;
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+    
+    // Update the count down every 1 second
+    var timer = 10;
+    let clicked = 0;
+
+    let label = document.querySelector(".aim-text");
+    let label2 = document.querySelector(".time-left");
+    let label3 = document.querySelector(".point-btn");
+    
+    var x = setInterval(function() {
+
+        
+        if (clicked > 0 && timer < 0)
+        {
+            timer--;
+            label2.innerHTML = `Time left:${timer}`;
+        }
+        
+
+    
+      document.getElementById("time").innerHTML = timer;
+        
+      // If the count down is over, write some text 
+      if (timer <= 0) {
+        //clearInterval(x);
+        //document.getElementById("time").innerHTML = "EXPIRED";
+    }
+
+    }, 1000);
 
 
-let label = document.querySelector(".aim-text");
-let label2 = document.querySelector(".point-btn");
 
-label2.addEventListener("click", handleClick);
+
+label3.addEventListener("click", handleClick);
 
 function handleClick() {
-    clicked+=1;
-    label.innerHTML = `Clicked: ${clicked} | Time left: __`;
+    if (timer > 0)
+    {
+        clicked+=1;
+    }
+    label.innerHTML = `Clicked:${clicked} |`;
 }
-
