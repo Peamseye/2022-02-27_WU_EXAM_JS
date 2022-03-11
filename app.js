8567bf536b540c4fd013a3cd69bb4a752cccecc0
Peamseye <aispotato587@gmail.com>
2022-03-11 20:12:15 +0100

    Save13

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
     
     // Update the count down every 1 second
-    var timer = 20;
+    var timer = 30;
     let clicked = 0;
 
     let label = document.querySelector(".aim-text");
